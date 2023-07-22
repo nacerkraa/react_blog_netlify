aa7c830ef2f376acd473c2789844a2f65bb426f6
nacerkraa <nacer.kraa@univ-constantine2.dz>
2023-07-22 23:10:14 +0100

    create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import NewPost from './components/NewPost';
+import PostPage from './components/PostPage';
+import About from './components/About';
+import Missing from './components/Missing';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function App() {
   return (
     
     <div className="App">
-      Hello app
+      <Header />
+      <Navbar />
+      <Home />
+      <NewPost />
+      <PostPage />
+      <About />
+      <Missing />
+      <Footer />
     </div>
   );
 }
